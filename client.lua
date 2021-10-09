613070f60be6e484187caa83871f695f61fcd1d6
EYOOOX <85031213+EYOOOX@users.noreply.github.com>
2021-10-09 14:14:51 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -5,47 +5,59 @@ ESX = nil
 Citizen.CreateThread(function()
 	while ESX == nil do
 		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
+		Citizen.Wait(10)
+    end
+    while ESX.GetPlayerData().job == nil do
+		Citizen.Wait(10)
+    end
+    if ESX.IsPlayerLoaded() then
+
+		ESX.PlayerData = ESX.GetPlayerData()
+
+    end
 end)
 
 PlayerData = {}
 
 RegisterNetEvent('esx:playerLoaded')
 AddEventHandler('esx:playerLoaded', function(xPlayer)
-	ESX.PlayerLoaded  = true
 	ESX.PlayerData    = xPlayer
 end)
 
-function datajob()
+RegisterNetEvent('esx:setJob')
+AddEventHandler('esx:setJob', function(job)
+	ESX.PlayerData.job = job
+end)
 
-	job = ESX.GetPlayerData().job.label
-	joblabel = ESX.GetPlayerData().job.grade_label
-	job2 = ESX.GetPlayerData().job2.label
-	job2label = ESX.GetPlayerData().job2.grade_label
-	
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(5000)
+		bank = ESX.GetPlayerData().accounts[1].money
+		black_money = ESX.GetPlayerData().accounts[2].money
+	end
+end)
+
+function datajob()
 	SetTextFont(0) 
 	SetTextScale(0.3, 0.3) 
 	SetTextColour(255, 255, 255, 255) 
 	SetTextEntry("STRING")
-	AddTextComponentString(job.. " - " ..joblabel.."~n~~n~"..job2.. " - " ..job2label)
+	AddTextComponentString(ESX.PlayerData.job.label.. " - "..ESX.PlayerData.job.grade_label.."~n~~n~"..ESX.PlayerData.job2.label.. " - " ..ESX.PlayerData.job2.label)
 	DrawText(0.87, 0.076) 
 	DrawRect(0.93, 0.108, 0.13, 0.08, 0, 0, 0, 200)
 end
   
 function datamoney()
-	cash = ESX.GetPlayerData().money
 	SetTextFont(0) 
 	SetTextScale(0.3, 0.3) 
 	SetTextColour(0, 255, 0, 255) 
 	SetTextEntry("STRING")
-	AddTextComponentString(cash.."$")
+	AddTextComponentString(ESX.PlayerData.money.."$")
 	DrawText(0.835, 0.163) 
 	DrawRect(0.905, 0.174, 0.18, 0.03, 0, 0, 0, 200)
 end
   
 function databank()
-	bank = ESX.GetPlayerData().accounts[1].money
 	SetTextFont(0) 
 	SetTextScale(0.3, 0.3) 
 	SetTextColour(19, 171, 216, 255) 
@@ -55,16 +67,11 @@ function databank()
 end
   
 function datasale()
-	black_money = ESX.GetPlayerData().accounts[2].money
 	SetTextFont(0) 
 	SetTextScale(0.3, 0.3) 
 	SetTextColour(255, 0, 0, 255) 
 	SetTextEntry("STRING")
-	--for i = 1, #ESX.PlayerData.accounts, 1 do
-		--if ESX.PlayerData.accounts[i].name == 'black_money'  then
-		AddTextComponentString(black_money.."$~n~")
-		--end
-	--end
+	AddTextComponentString(black_money.."$~n~")
 	DrawText(0.949, 0.163) 
 end
   
